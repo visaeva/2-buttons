41d0abfdb93fe328d932fcc8d56f7a687ff45158
Victoria Isaeva <visaeva0302@gmail.com>
2023-03-14 19:03:17 +0300

    Initial Commit

---
+++ 2 кнопки/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2 кнопки
+//
+//  Created by Victoria Isaeva on 14.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
