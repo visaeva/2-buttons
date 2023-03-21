8e547c6e0d1b0e79899e2dd6af9320750a7b4ad2
Victoria Isaeva <visaeva0302@gmail.com>
2023-03-21 13:19:24 +0300

    Создан счетчик со значениями +-1, кнопкой сброса. Так же на экране отображается история изменений, время и дата.

--- 2 кнопки/ViewController.swift
+++ 2 кнопки/ViewController.swift
@@ -8,12 +8,88 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet private weak var labelCount: UILabel!
+    
+    private var labelNumer = 0
+    
+    
+    
+    @IBAction private func buttonPlus(_ sender: Any) {
+        labelNumer = labelNumer + 1
+        labelCount.text = String(labelNumer)
+        
+        TextChanges.isEditable = false
+        TextChanges.isSelectable = false
+        
+        if labelNumer > 0 {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+            let date = formatter.string(from: Date())
+            
+            startText = "\(date) : значение изменено на +1 \n"
+            TextChanges.text.append(startText)
+            TextChanges.scrollRangeToVisible(NSMakeRange(TextChanges.text.count - 1, 1))
+        }
     }
-
-
+    
+    @IBAction private func buttonMinus(_ sender: Any) {
+        labelNumer = labelNumer - 1
+        labelCount.text = String(labelNumer)
+        
+        if labelNumer <= 0 {
+            labelNumer = 0
+            labelCount.text = String(labelNumer)
+            
+        }
+        
+        if labelNumer <= 0 {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+            let date = formatter.string(from: Date())
+            startText = "\(date): попытка изменить счетчик меньше 0 \n"
+            TextChanges.text.append(startText)
+            TextChanges.scrollRangeToVisible(NSMakeRange(TextChanges.text.count - 1, 1))
+        }
+        if labelNumer != 0 {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+            let date = formatter.string(from: Date())
+            startText = "\(date) : значение изменено на -1 \n"
+            TextChanges.text.append(startText)
+            TextChanges.scrollRangeToVisible(NSMakeRange(TextChanges.text.count - 1, 1))
+            
+            
+        }
+        
+    }
+    
+    @IBAction func deleteCount(_ sender: Any) {
+        labelCount.text = "0"
+        labelNumer = 0
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        let date = formatter.string(from: Date())
+        startText = "\(date): значение сброшено \n"
+        TextChanges.text.append(startText)
+        TextChanges.scrollRangeToVisible(NSMakeRange(TextChanges.text.count - 1, 1))
+    }
+    
+    @IBOutlet weak var TextChanges: UITextView!
+    var startText = "История изменений:"
+    
+    // @IBOutlet weak var textDate: UITextView!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        labelCount.text = String(labelNumer)
+        super.viewDidLoad()
+        TextChanges.text = startText
+        
+    }
+    
+    
 }
 
